0e4a539a6605b9463b0278d1036b5eedebfd5587
Jonathan Leung <me@jonl.org>
2023-01-28 18:41:50 -0500

    Change to first attempt at regex

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import re
 
-with open("test.txt", "r") as f:
+with open("hard.txt", "r") as f:
     file_contents = f.read()
 
-pattern = re.compile(r"\w*")
+pattern = re.compile(r"-\s\w{3},\s\w{3}\s\d{1,2},\s\d{4}\n(?:(?!-\s\w{3},\s\w{3}\s\d{1,2},\s\d{4}\n).)*", re.MULTILINE |re.DOTALL | re.IGNORECASE);
 
 for match in pattern.finditer(file_contents):
     print(match.group())
