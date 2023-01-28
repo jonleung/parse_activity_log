5fb92968a484ec2e6875eeff6db99fb8e9b5c90a
Jonathan Leung <me@jonl.org>
2023-01-28 18:44:51 -0500

    Print all matches

--- main.py
+++ main.py
@@ -5,5 +5,6 @@ with open("hard.txt", "r") as f:
 
 pattern = re.compile(r"-\s\w{3},\s\w{3}\s\d{1,2},\s\d{4}\n(?:(?!-\s\w{3},\s\w{3}\s\d{1,2},\s\d{4}\n).)*", re.MULTILINE |re.DOTALL | re.IGNORECASE);
 
-for match in pattern.finditer(file_contents):
-    print(match.group())
+for a, match in enumerate(pattern.finditer(file_contents)):
+    print(f"### Match {a+1}:\n{match.group()}")
+
