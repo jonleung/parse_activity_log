baca16c47e0d8f608aa2c14d284def0c4a0d7565
Jonathan Leung <me@jonl.org>
2023-01-28 21:20:21 -0500

    Finish MVP of just printing out the parsed data

--- main.py
+++ main.py
@@ -28,5 +28,10 @@ for a, date_match in enumerate(date_regex.finditer(file_contents)):
 
         # remove extra spaces from first line
         task_first_line = re.sub(r"\s+", " ", task_first_line).strip()
+        # print(f"## Task Match {b+1}: \t '{hashtag}' \t Deets: '{task_first_line}'\n{task_level_string}")
+        print(f"\nTask Match {b+1}: '{hashtag}', {task_first_line}")
 
-        print(f"## Task Match {b+1}: \t '{hashtag}' \t Deets: '{task_first_line}'\n{task_level_string}")
+        time_regex = re.compile(r"(\s{4}-\s(\d{1,2}:\d{1,2}\s*\w{2})\s*-\s*(\d{1,2}:\d{1,2}\s*\w{2})\s*$\n)", RE_SETTINGS)
+        for c, time_match in enumerate(time_regex.finditer(task_level_string)):
+            print(f"\t Time Match {c+1}: {time_match.group(2)} - {time_match.group(3)}")
+
